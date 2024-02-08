5533a645f43dea55ab01d0bea16ce132b506d7fe
KamolBaxodirov <k98054815@gmail.com>
2024-02-08 10:43:22 +0500

    js ga kirish

---
+++ js/main.js
@@ -0,0 +1,178 @@
+// "use strict"
+
+// //interpolatsiya
+
+// // const channel = prompt("Sizning sevimli saytingiz?")
+// // console.log("https://www."+channel+".com")
+
+
+// // console.log(`https://www.${channel}.com`)
+
+// // const m =prompt("ismingiz nima?") // kamol
+// // const n = prompt("Familiyangiz qanday?")  // baxadirov
+// // const h  = prompt("yoshingiz nechida?")  // 15
+
+// // console.log(`mening ismim:${a}, mening familiyam: ${b}, yoshim esa ${c}`)
+ 
+// // console.log(`men ${2024-c}-yilda tug'ilganman`)
+
+
+// // ism 
+// // fam 
+// // ochestva
+// // yosh
+// // tugilgan- shahar
+
+
+// // Men, 'ism', 'fam', 'ochestva', 'yosh', daman, 'Tugilgan shahar', da -tugilganman  
+
+// // const a = prompt("Familiyangiz qanday")
+// // const b = prompt("Ismingiz nima")
+// // const c = prompt("Otchestvangiz qanday")
+// // const d =prompt("nechi yoshsiz") 
+
+// // console.log(`mening familiyam:${a},`)
+// // console.log(`ismim${b},`)
+// // console.log(`Otchestvam${c},`)
+// // console.log(`men ${2024-d}-yilda `)
+// // // console.log(`${}-oyda tug'ilganman`)
+
+// // const h =confirm("yahshimisiz")
+
+
+// //          6.02.2024  operatorlar 
+
+// // let a = "Java" // string
+// // let b = "Script"  //string
+// // let c = 13
+// // let d = "13"
+
+
+
+// // console.log(a+b);
+// // console.log(b+c);
+// // console.log(+d+c);
+
+// // // incr; decr 
+
+// // let incr = 10;
+// // let decr = 10;
+
+// // incr ++;
+// // decr --;
+
+
+// // console.log(incr++);
+// // console.log(decr--);
+
+// // console.log(++incr);
+// // console.log(--decr);
+// //  const j =prompt("yoshingiz nechida");
+// // let h = +j;
+// // h++;
+// // h++;
+// // console.log( h)
+
+
+// // let p = 17
+// // let k = 7
+
+// // console.log(p%k)
+
+// // const f =prompt("a-sonini kiriting") //15
+// // const s =prompt("b-sonini kiriting") //"2"
+// // let m = +f%+s
+// // console.log(m)
+//  //"14"//"22"
+// // const у =prompt("a-sonini kiriting")
+// // const е =prompt("b-sonini kiriting") 
+// // let к = +y
+// // k++
+
+// // let c = +e
+// // c-- 
+
+
+// // calculator
+// //   13
+// //   45
+// //   -
+// //   +
+// //   *
+// //   /
+// //   %
+// //   ++
+// //   --
+
+
+// const m =prompt("a-sonini kiriting")
+// const k =prompt("b-sonini kiriting") 
+// let i = +m 
+// i++
+// let w = +k 
+// w-- 
+
+// console.log(i%w)
+
+
+// const p =prompt("c-sonini kiriting")
+// const l =prompt("d-sonini kiriting") 
+// let q = +p 
+// q++
+// let e = +l
+// e-- 
+
+// console.log(q+e)
+
+
+// const u =prompt("e-sonini kiriting")
+// const j =prompt("f-sonini kiriting") 
+// let a = +u
+// a++
+// let s = +j
+// s-- 
+
+// console.log(a-s)
+
+
+// const d =prompt("j-sonini kiriting")
+// const f =prompt("h-sonini kiriting") 
+// let g = +d
+// g++
+// let h = +f
+// h-- 
+
+// console.log(g*h)
+
+// const z =prompt("i-sonini kiriting")
+// const x =prompt("k-sonini kiriting") 
+// let v= +z
+// v++
+// let b = +x
+// b-- 
+
+// console.log(v/b)
+
+
+
+// console.log((i%w)+(q+e)+(a-s)+(g*h)+(v/b))
+
+
+
+/////   08.02.2024  
+
+console.log(5*5 === 25) 
+console.log(10!=10)
+// ||(или)+  &&(и)*
+
+        //  true(1)   false(0)
+console.log(10==10 && 10>11) 
+console.log(13<10 || 10==10 && 1<0 || true || true)
+
+console.log(13%5>9 && 11/5<3) 
+console.log(18<x && 25>x)
+
+let s = {
+    rang: 12,
+    da: {}
+}
